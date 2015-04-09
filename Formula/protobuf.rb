cdf4aaffadfd5634babcc60f79831e241505093e
Tim Emiola <temiola@google.com>
2015-04-09 09:12:52 -0700

    Fixes the protobuf.rb

--- Formula/protobuf.rb
+++ Formula/protobuf.rb
@@ -1,4 +1,4 @@
-class Protobuf3 < Formula
+class Protobuf < Formula
   homepage "https://github.com/google/protobuf/"
   url "https://github.com/google/protobuf/archive/v3.0.0-alpha-2.tar.gz"
   sha256 "46df8649e2a0ce736e37f8f347f92b32a9b8b54d672bf60bd8f6f4d24d283390"
